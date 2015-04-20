08cc09e42ae1a1b5e0e62c7445c460cf4af9b58c
altair <gcthuan@apcs.vn>
2015-04-20 22:58:56 +0700

    Add more attributes to sticker model

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,5 +1,14 @@
 class Sticker
   include Mongoid::Document
-  field	:uri
+  field	:frame_count, type: Integer
+  field :frame_rate, type: Integer
+  field :frames_per_col, type: Integer
+  field :frames_per_row, type: Integer
+  field :height, type: Integer
+  field :width, type: Integer
+  field :source_uri
+  field :uri
+  field :sprite_uri
+  field :padded_sprite_uri
   embedded_in :pack
 end
